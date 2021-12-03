be5eb50f85a5627ed7581c1d3d53eb01c71630cf
boginni <brunnomarqueszr1@gmail.com>
2021-12-03 18:04:25 -0300

    remove brunno marques

--- brunno marques/vendas/lib/app/modules/auth/auth_store.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_triple/flutter_triple.dart';
-
-class AuthStore extends NotifierStore<Exception, int> {
-
-  AuthStore() : super(0);
-
-}
--- brunno marques/vendas4/android/app/src/main/kotlin/com/example/vendas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vendas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
