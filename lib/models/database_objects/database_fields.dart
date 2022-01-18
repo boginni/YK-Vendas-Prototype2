decbff24cdcebac7353e15e79f5d24a4d82202c3
boginni <brunnomarqueszr1@gmail.com>
2022-01-18 18:40:51 -0300

    Implementação do DropdownSaved

--- lib/models/database_objects/database_fields.dart
+++ lib/models/database_objects/database_fields.dart
@@ -16,59 +16,68 @@ class DropDownSavedValue {
   }
 
   @override
-  bool operator ==(Object other) => other is DropDownSavedValue && other.id == id;
+  bool operator ==(Object other) =>
+      other is DropDownSavedValue && other.id == id;
 
   @override
   int get hashCode => id.hashCode;
-
 }
 
 class DropDownSaved extends StatefulWidget {
+  static const String tabelaPreco = 'VW_TABELA_PRECO';
 
   final String table;
   DropDownSavedValue? currentValue;
+  Function(DropDownSavedValue? item)? onChange;
 
-  DropDownSaved(this.table, {Key? key, this.currentValue}) : super(key: key);
+  DropDownSaved(this.table, {Key? key, this.currentValue, this.onChange}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => DropDownSavedState();
 }
 
-
 class DropDownSavedState extends State<DropDownSaved> {
-
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    Future<List<DropDownSavedValue>> getList() async{
-      final Database db = await DatabaseLocal.getDatabase();
+    Future<List<DropDownSavedValue>> getList() async {
+      List<DropDownSavedValue> list = [];
+      try {
+        final Database db = await DatabaseLocal.getDatabase();
 
-      final List<Map<String, dynamic>> maps = await db.query(widget.table);
+        final List<Map<String, dynamic>> maps = await db.query(widget.table);
 
-      final list = List.generate(maps.length, (i) {
-        final tp = DropDownSavedValue(maps[i]['ID']);
-        tp.nome = maps[i]['NOME'];
-        return tp;
-      });
+        list = List.generate(maps.length, (i) {
+          final tp = DropDownSavedValue(maps[i]['ID'], nome: maps[i]['NOME']);
+          return tp;
+        });
+      } catch (e) {
+        debugPrint(e.toString());
+      }
 
       return list;
     }
 
-    update(final DropDownSavedValue? item){
-
+    update(final DropDownSavedValue? item) {
       setState(() {
         widget.currentValue = item;
       });
+
+      widget.onChange!(item);
+
+
     }
 
     return FutureBuilder(
       future: getList(),
       builder: (BuildContext context,
           AsyncSnapshot<List<DropDownSavedValue>> snapshot) {
-        final list = snapshot.data!;
+        if (!snapshot.hasData) {
+          return const Text('Carregando');
+        }
 
+        final list = snapshot.data!;
 
         return DropdownButton<DropDownSavedValue>(
           value: widget.currentValue,
@@ -82,8 +91,7 @@ class DropDownSavedState extends State<DropDownSaved> {
             update(item);
           },
         );
-      },);
+      },
+    );
   }
-
-
 }
--- lib/screens/tela_principal/tela_pedido/tela_tabela_de_preco.dart
+++ lib/screens/tela_principal/tela_pedido/tela_tabela_de_preco.dart
@@ -1,45 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:forca_de_vendas/common/tiles/default_tiles.dart';
 import 'package:forca_de_vendas/models/database_local.dart';
+import 'package:forca_de_vendas/models/database_objects/database_fields.dart';
 import 'package:sqflite/sqflite.dart';
-
-/*
-
-var _valores = ['valor', 'valor2', 'valor3'...];
-DropDownButton<String>(
-
-             items : _valores.map((String dropDownStringItem) {
-
-              return DropdownMenuItem<String>(
-                   value : dropDownStringItem,
-                   child : Text(dropDownStringItem),
-            );
-           }).toList(),
-
-           onChanged: () {
-                setState( ()  {  //...seu codigo      });
-           },
-);
-
- */
-
-class TabelaPreco {
-  String nome = '';
-  final int id;
-
-  TabelaPreco(this.id);
-
-  @override
-  String toString() {
-    return "$id -> $nome";
-  }
-
-  @override
-  bool operator ==(Object other) => other is TabelaPreco && other.id == id;
-
-  @override
-  int get hashCode => id.hashCode;
-}
 
 class TelaTabelaPreco extends StatefulWidget {
   static const routeName = '/telaTabelaPreco';
@@ -51,11 +14,9 @@ class TelaTabelaPreco extends StatefulWidget {
 }
 
 class TelaTabelaPrecoState extends State<TelaTabelaPreco> {
+  DropDownSavedValue? tabela;
 
-
-  TabelaPreco? tabela;
-
-  void setTabela(TabelaPreco? x) async {
+  void setTabela(DropDownSavedValue? x) async {
     setState(() {
       tabela = x;
     });
@@ -66,21 +27,20 @@ class TelaTabelaPrecoState extends State<TelaTabelaPreco> {
     Navigator.of(context).pop(context);
   }
 
-
-  Future<List<TabelaPreco>> setup(final idVisita) async{
-    final list = await getListTabelaPreco();
-    try{
-      tabela = await getTabelaPreco(idVisita);
-    } catch (e){
-
-    }
-    return list;
-  }
-  
-  
   @override
   Widget build(BuildContext context) {
     final int idVisita = ModalRoute.of(context)!.settings.arguments as int;
+
+    Future<int> setup() async {
+      try {
+        tabela = await getTabelaPreco(idVisita);
+      } catch (e) {
+        debugPrint(e.toString());
+
+      }
+
+      return 1;
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -93,33 +53,24 @@ class TelaTabelaPrecoState extends State<TelaTabelaPreco> {
       ),
       body: Card(
         child: FutureBuilder(
-          future: setup(idVisita),
-          builder: (BuildContext context,
-              AsyncSnapshot<List<TabelaPreco>> snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
+          future: setup(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+
+            if (snapshot.connectionState != ConnectionState.done) {
+              return const Text('Carregando');
             }
 
-            final list = snapshot.data!;
+            debugPrint(tabela.toString());
 
             return ListView(
               children: <Widget>[
 
-
-                DropdownButton<TabelaPreco>(
-                  value: tabela,
-                  items: list.map((TabelaPreco item) {
-                    return DropdownMenuItem<TabelaPreco>(
-                      value: item,
-                      child: Text(item.toString()),
-                    );
-                  }).toList(),
-                  onChanged: (item) {
-                    setTabela(item);
-                  },
+                DropDownSaved(
+                  DropDownSaved.tabelaPreco,
+                  currentValue: tabela,
+                  onChange: (i) => setTabela(i),
                 ),
+
 
                 TextButton(
                   onPressed: tabela != null ? () => saveTabela(idVisita) : null,
@@ -128,12 +79,13 @@ class TelaTabelaPrecoState extends State<TelaTabelaPreco> {
                     style: TextStyle(color: Colors.black),
                   ),
                 )
-
-
-
-
               ],
             );
+
+            try {} catch (e) {
+              debugPrint(e.toString());
+              return Text(e.toString());
+            }
           },
         ),
       ),
@@ -141,38 +93,22 @@ class TelaTabelaPrecoState extends State<TelaTabelaPreco> {
   }
 }
 
-Future<List<TabelaPreco>> getListTabelaPreco() async {
+Future<DropDownSavedValue?> getTabelaPreco(int idVisita) async {
   final Database db = await DatabaseLocal.getDatabase();
 
-  final List<Map<String, dynamic>> maps = await db.query('VW_TABELA_PRECO');
+  final List<Map<String, dynamic>> maps = await db
+      .query('VW_VISITA', where: 'ID_VISITA = ?', whereArgs: [idVisita]);
 
   final list = List.generate(maps.length, (i) {
-    final tp = TabelaPreco(maps[i]['ID']);
-    tp.nome = maps[i]['NOME'];
-    return tp;
-  });
-
-  return list;
-}
-
-
-Future<TabelaPreco?> getTabelaPreco(int idVisita) async {
-  final Database db = await DatabaseLocal.getDatabase();
-
-  //NOME_TABELA
-  //ID_TABELA_PRECOS
-  final List<Map<String, dynamic>> maps = await db.query('VW_VISITA', where: 'ID_VISITA = ?', whereArgs: [idVisita]);
-
-  final list = List.generate(maps.length, (i) {
-    final tp = TabelaPreco(maps[i]['ID_TABELA_PRECOS']);
-    tp.nome = maps[i]['NOME_TABELA'];
+    final tp = DropDownSavedValue(maps[i]['ID_TABELA_PRECOS'], nome: maps[i]['NOME_TABELA']);
     return tp;
   });
 
   return list[0];
 }
 
-Future<void> insertTabelaPreco(final int idVisita, final TabelaPreco x) async {
+Future<void> insertTabelaPreco(
+    final int idVisita, final DropDownSavedValue x) async {
   try {
     final db = await DatabaseLocal.getDatabase();
     await db.insert(
@@ -180,11 +116,7 @@ Future<void> insertTabelaPreco(final int idVisita, final TabelaPreco x) async {
       {'ID_VISITA': idVisita, 'ID_TABELA_PRECOS': x.id},
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
-
   } catch (e) {
     debugPrint(e.toString());
   }
 }
-
-
-
